4526678898df6770ccafa4e15b5633ac0b4f7d0e
Famá Román <famaroman@gmail.com>
2022-07-22 22:42:14 -0300

    change system the password for sha256

--- scripts/util.py
+++ scripts/util.py
@@ -1,8 +1,8 @@
-import os, pandas as pd
+import os, pandas as pd, base64 as b64
 from tkinter import filedialog as filed
 from tkinter import Tk
 from time import localtime
-from bcrypt import gensalt, hashpw
+from hashlib import sha256
 from cryptography.fernet import Fernet
 if __name__ != '__main__': from scripts.bd import bd
 else: from bd import bd
@@ -13,6 +13,11 @@ class util(bd):
     def generateKey(self, path):
         with open(path, 'wb') as key:
             key.write(Fernet.generate_key())
+
+    def generateClavePorPass(path, mensaje: str):
+        b6432b = b64.urlsafe_b64encode(sha256(mensaje.encode()).digest())
+        with open(path, 'wb') as key:
+            key.write(b6432b)
 
     def encryptData(self, data: bytes, keypath):
         with open(keypath, 'rb') as keyfile:
@@ -25,20 +30,13 @@ class util(bd):
         return f.decrypt(data)
 
 #password
-    def createPassword(self, password, salt):
-        '''Salt == 0 se genera uno nuevo'''
-        password = bytes(password, 'utf-8')
-        if salt == 0:
-            salts = gensalt(16)
-        else:
-            salts = salt
-        hashed = hashpw(password, salts)
-        return (hashed, salts)
+    def createPassword(self, password: str):
+        return sha256(password.encode()).digest()
 
     def passSave(self, pathBD, inputPass):
-        (self.password, self.salt) = self.createPassword(inputPass, 0)
-        print(self.password, self.salt)
-        self.query(pathBD, "insert into userconfigs (password, salt) values (?, ?)", (self.password, self.salt))
+        self.password = self.createPassword(inputPass, 0)
+        print(self.password)
+        self.query(pathBD, "insert into userconfigs (password) values (?)", (self.password))
 
     def passExistverification(self, pathBD, inputPass):
         if os.path.exists(pathBD) == False:
@@ -47,18 +45,18 @@ class util(bd):
             return True
         else:
             if self.verifyBD(pathBD):
-                (self.password, self.salt) = self.query(pathBD, 'select password, salt from userconfigs', returnData=True)
+                self.password = self.query(pathBD, 'select password from userconfigs', returnData=True)
                 return True
             else:
                 return False
 
     def passVerification(self, pathBD, inputPass):
         if self.passExistverification(pathBD, inputPass):
-            return self.createPassword(inputPass, self.salt) == (self.password, self.salt)
-            del self.password, self.salt
+            return self.createPassword(inputPass) == self.password
+            del self.password
         else:
             return [False, False]
-            del self.password, self.salt
+            del self.password
 
 #funciones utiles
     def browsePath(self, browseCarpeta, title, mainTypeText: str=..., mainType: str=...):
